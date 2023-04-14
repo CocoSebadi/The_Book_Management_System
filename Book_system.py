0c977ef49f832ebd568f00ceeb6b0ce70f9fe89e
Nmatsipa12@gmail.com <129933617+TokyoN12@users.noreply.github.com>
2023-04-14 14:10:06 +0200

    Added my delete function

--- Book_system.py
+++ Book_system.py
@@ -1,1 +1,19 @@
-
+# Function to delete a book from the text file
+def delete_book():
+    isbn = input("Enter the isbn of the book to delete: ")
+    try:
+        with open(book_path, "r") as file:
+            lines = file.readlines()
+        with open(book_path, "w") as file:
+            found = False
+            for line in lines:
+                if isbn in line:
+                    found = True
+                else:
+                    file.write(line)
+            if found:
+                print("Book deleted successfully!")
+            else:
+                print("Book not found.")
+    except FileNotFoundError:
+        print("No books found.")
