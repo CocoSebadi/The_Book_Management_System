46d29ffdccddb8296d246b0dcec12b1de20fe309
zamaphiri22@gmail.com <zamaphiri22@gmail.com>
2023-04-14 14:05:41 -0700

    Added the main function

--- Book_system.py
+++ Book_system.py
@@ -1,1 +1,31 @@
+def main():
+    while True:
+        print("\nWelcome to the book management system!")
+        print("1. Display all books")
+        print("2. Add a book")
+        print("3. Search for a book")
+        print("4. Edit a book")
+        print("5. Delete a book")
+        print("6. Quit")
+
+        choice = input("Enter your choice (1-6): ")
+        if choice == "1":
+            display_books()
+        elif choice == "2":
+            add_book()
+        elif choice == "3":
+            search_book()
+        elif choice == "4":
+            edit_book()
+        elif choice == "5":
+            delete_book()
+        elif choice == "6":
+            print("Thank you for visiting the book store, Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main()
+        
 
